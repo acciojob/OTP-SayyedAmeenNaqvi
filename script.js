5992647d63f5ff1e9e5ef716d96e14f7787b07ae
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-09-17 09:03:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    // allow only digits
+    if (!/^\d$/.test(e.target.value)) {
+      e.target.value = "";
+      return;
+    }
+    
+    if (input.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      }
+    }
+  });
+});
